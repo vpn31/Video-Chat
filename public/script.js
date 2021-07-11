412d59c9a058e2bd04fa4745cf462d606d46c67a
Vatsal Pravin <pravin.vats20@gmail.com>
2021-07-11 12:45:25 +0530

    make it better

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket=io('/');
 const videoGrid=document.getElementById('video-grid');
 const myPeer=new Peer(undefined,{
   secure: true, 
-  host: 'video-call-clone-app.herokuapp.com', 
+  host: 'video-call-clone-app.herokuapp.com/', 
   port: 443,
 });
 window.peer = myPeer;
